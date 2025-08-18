449a276a61453de2be0365059dab1ac6111bde72
Al Zaid <alzaidj9044@gmail.com>
2025-08-18 21:18:24 +0530

    'update'

---
+++ conditional.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class conditional {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        // if(age > 18){
+        // System.out.println("ADULT");
+        // }
+        // else{
+        // System.out.println("NOT ADULT");
+        // }
+        // if (age>5) {
+        // System.out.println("good");
+
+        // } else {
+        // System.out.println("bad");
+
+        // }
+        if (x % 2 == 0) {
+            System.out.println("even");
+
+        } else {
+            System.out.println("odd");
+        }
+
+    }
+}
