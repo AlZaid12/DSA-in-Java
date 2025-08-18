cd0df9ad8560eafeb865df5abde572a71e2ba818
Al Zaid <alzaidj9044@gmail.com>
2025-08-18 21:13:18 +0530

    Added basic.java file

---
+++ basic.java
@@ -0,0 +1,22 @@
+
+public class basic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("hello worlf with");
+        System.out.println("*\n**\n***\n****");
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+        int c = 8;
+        int d = 2;
+        int diff = d - 8;
+        System.out.println(diff);
+        int mul = a * b;
+        System.out.println(mul);
+        int ans = (a * b) / (a - b);
+        System.out.println(ans);
+
+    }
+}
