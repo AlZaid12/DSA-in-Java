608e095299c64386a2dd97c1af385dff3d9ad55b
Al Zaid <alzaidj9044@gmail.com>
2026-02-06 01:58:40 +0530

    update

--- Twodarrays.java
+++ Twodarrays.java
@@ -13,7 +13,7 @@
                numbers [i][j] = sc.nextInt();
             }
         }
-        //output
+        //output -:
         for( int i=0; i<rows; i++){
             for (int j=0; j<cols; j++) {
                  System.out.println(numbers[i][j] + " ");
